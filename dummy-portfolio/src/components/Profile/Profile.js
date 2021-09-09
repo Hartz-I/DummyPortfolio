cd76fdd55dd26085a54b682d041f35894c940fec
Ahnaf Sadik Inan <ahnafsadikinan@gmail.com>
2021-09-09 16:30:39 +0600

    added orders page

--- dummy-portfolio/src/components/Profile/Profile.js
+++ dummy-portfolio/src/components/Profile/Profile.js
@@ -4,7 +4,11 @@ import classes from "./Profile.module.css";
 const profile = (props) => {
   return (
     <div className={classes.Profile}>
-      <img className={classes.Profile__Pic} src={props.img}></img>
+      <img
+        className={classes.Profile__Pic}
+        src={props.img}
+        alt="profile__pic"
+      ></img>
       <div className={classes.Profile__Description}>
         Hi! I'm Ahnaf Sadik Inan! A professional React Developer for your
         frinedly nabourhood!
